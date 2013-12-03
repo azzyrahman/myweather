dd9e45b538c12b6f90687c403e8478f1cb0f7192
Azizur Rahman <arahman@mac-dan.corp.theweather.com.au>
2013-12-03 11:41:23 +1100

    latest4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ Myweather::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   #config.assets.precompile += %w( custom.css radar-animator.css)
-
+  #arahman
+  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
